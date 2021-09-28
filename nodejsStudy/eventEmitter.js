2a64a4c61c38aceb20aeafa8e60a683a8416e533
정찬욱 <70217158+JinAru16@users.noreply.github.com>
2021-09-28 20:04:58 +0900

    경로수정

---
+++ nodejsStudy/eventEmitter.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on('test', () => {
+    console.log('1');
+})
+
+myEmitter.on('test', () => {
+    console.log('2');
+})
+myEmitter.on('test', () =>{
+    console.log('3');
+})
+
+myEmitter.emit('test');
