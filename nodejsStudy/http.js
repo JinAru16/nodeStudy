4f0aea7210f3a16073f25b9c616b5647112eb4fa
정찬욱 <70217158+JinAru16@users.noreply.github.com>
2021-09-28 21:29:43 +0900

    hello world 출력해보기

--- nodejsStudy/http.js
+++ nodejsStudy/http.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
-let server = http.createServer();
-server.listen(3000);
+let server = http.createServer(function (request, response) {
+    response.end('<h1>Hello World!</h1>');
+});
+server.listen(3000);  // 3000은 포트번호를 의미
 
-// 3000은 포트번호를 의미
+
