ee705a35d906f1b13161bdb535ea5a20e747d71f
정찬욱 <70217158+JinAru16@users.noreply.github.com>
2021-09-29 21:50:27 +0900

    request
    request 받은 url의 path 출력하기

---
+++ nodejsStudy/routing.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+let server = http.createServer(function (request, response) {
+    console.log(request.url);
+
+    //response.end('<h1>Hello World</h1>');
+
+});
+
+server.listen(3000);
+
